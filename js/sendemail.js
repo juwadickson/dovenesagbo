ccf1b462f20b1b9a051d135984bfcaae0c8a891d
Juwa Dickson <ayoomojuwa@yahoo.com>
2024-11-04 23:33:49 +0100

    all pages added

--- js/sendemail.js
+++ js/sendemail.js
@@ -3,3 +3,13 @@ function sendEmail(courseName) {
     const body = encodeURIComponent(`I would like to enroll in the course: ${courseName}.`);
     window.location.href = `mailto:sydoveacademy@gmail.com?subject=${subject}&body=${body}`;
 }
+
+function sendEmails(event) {
+    event.preventDefault();
+    const notification = document.getElementById("notification");
+    notification.style.display = "block";
+    setTimeout(() => {
+      notification.style.display = "none";
+    }, 3000);
+  }
+  
